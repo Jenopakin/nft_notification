03970d7e02187d3a1fb314239e193eb7f7a14f78
Jenopakin <147024728+Jenopakin@users.noreply.github.com>
2024-09-23 01:30:55 +0700

    Update NFT_notification_v3.py

--- NFT_notification.py
+++ NFT_notification.py
@@ -55,7 +55,7 @@ def check_listings_with_selenium():
 
     try:
         # Explicit wait to make sure the listings load
-        WebDriverWait(driver, 10).until(
+        WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.TAG_NAME, "dt"))
         )
     except Exception as e:
